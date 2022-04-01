d4bf54e964f055daa2e3b9b0883268ea35eb39e9
Kelly Jefferies <k.jefferies@gmail.com>
2022-03-31 20:53:42 -0500

    completed delete function

--- server.js
+++ server.js
@@ -11,15 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 app.use(express.static('develop/public'))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'develop/public/index.html'))
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
 });
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'develop/public/index.html'))
-// });
 
 app.get('/api/notes', (req, res) => {
     const json = fs.readFileSync('./develop/db/db.json', 'utf-8')
@@ -34,7 +32,13 @@ app.post('/api/notes', (req, res) => {
     res.send(JSON.stringify(req.body));
 })
 
-app.delete()
+app.delete('/api/notes/:id', function(req, res) {
+    let json = fs.readFileSync('./develop/db/db.json', 'utf-8')
+    let dB = JSON.parse(json)
+    dB = dB.filter(i => i.id != req.params.id)
+    fs.writeFileSync('./develop/db/db.json', JSON.stringify(dB))
+    res.send(req.params.id);
+})
 
 // Listener
 app.listen(port, () =>
